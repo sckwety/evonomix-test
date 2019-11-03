02654d5a659dc8b00cd590675c06bffca655666b
Cosmin <cosmin@creative-ones.com>
2019-11-04 00:03:03 +0200

    auth with admin

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'gender' => 'other',
+            'birthdate' => date('Y-m-d'),
+            'email' => 'admin@admin.com',
+            'email_verified_at' => date('Y-m-d'),
+            'is_admin' => 1,
+            'password' => bcrypt('admin@admin.com'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
